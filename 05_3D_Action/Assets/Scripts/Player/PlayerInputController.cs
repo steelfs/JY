2dbd530b86d531515acd37e52ba793b039e81aa0
inchimyeong <125725323+steelfs@users.noreply.github.com>
2023-07-18 10:01:48 +0900

    3D_Action 플레이어 카메라기준으로 회전시키기
    transfirm,.rotation = quaternion.Slerp(transform.rotation, targetRotation, time.deltatime* rotateSpeed)
    
    Quaternion targetrotation = quaternion.LookRotation(inputDir);

--- 05_3D_Action/Assets/Scripts/Player/PlayerInputController.cs
+++ 05_3D_Action/Assets/Scripts/Player/PlayerInputController.cs
@@ -10,7 +10,7 @@ public class PlayerInputController : MonoBehaviour
 
     public float walkSpeed = 3.0f;
     public float runSpeed = 6.0f;
-    float currentSpeed = 5.0f;
+    float currentSpeed = 3.0f;
 
     int speedHash = Animator.StringToHash("Speed");
 
@@ -33,15 +33,22 @@ public class PlayerInputController : MonoBehaviour
             {
                 case MoveMode.Run:
                     currentSpeed = runSpeed;
+                //   anim.SetFloat(speedHash, 1.0f);
                     break;
                 case MoveMode.Walk:
+                    currentSpeed = walkSpeed;
+                //    anim.SetFloat(speedHash, 0.3f);
+                    break;
+                default:
                     currentSpeed = walkSpeed;
                     break;
             }
         }
     }
 
-    Vector3 inputDir;
+    Vector3 inputDir = Vector3.zero;
+    Quaternion targetRotation = Quaternion.identity;
+    public float turnSpeed = 10.0f;
 
     CharacterController characterController;
     private void Awake()
@@ -59,8 +66,25 @@ public class PlayerInputController : MonoBehaviour
 
     }
 
-   
+    private void OnDisable()
+    {
+        action.Player.Move.performed -= OnMove;
+        action.Player.Move.canceled -= OnMove;
+        action.Player.ChangeMode.performed -= OnMoveModeChange;
+        action.Player.Disable();
+    }
+    private void Start()
+    {
+        MoveSpeedMode = MoveMode.Run;
+    }
+    private void Update()
+    {
+        characterController.Move(Time.deltaTime * currentSpeed * inputDir); // 비교적 수동에 가까운 느낌       
+                                                                            //  characterController.SimpleMove(currentSpeed * inputDir); // 자동에 가까운 느낌
 
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * turnSpeed); 
+        
+    }
     private void OnMoveModeChange(UnityEngine.InputSystem.InputAction.CallbackContext _)
     {
         switch (MoveSpeedMode)
@@ -71,22 +95,7 @@ public class PlayerInputController : MonoBehaviour
             case MoveMode.Walk:
                 MoveSpeedMode = MoveMode.Run;
                 break;
-        }   
-    }
-
-    private void OnDisable()
-    {
-        action.Player.Move.performed -= OnMove;
-        action.Player.Move.canceled -= OnMove;
-        action.Player.Disable();
-    }
-
-    private void Update()
-    {
-        characterController.Move(Time.deltaTime * currentSpeed * inputDir); // 비교적 수동에 가까운 느낌
- 
-      //  characterController.SimpleMove(currentSpeed * inputDir); // 자동에 가까운 느낌
-        
+        }
     }
     private void OnMove(UnityEngine.InputSystem.InputAction.CallbackContext context)
     {
@@ -95,24 +104,29 @@ public class PlayerInputController : MonoBehaviour
         inputDir.z = input.y;
         inputDir.y =  -2.0f;// 안내려갈 경우
 
-        float moveSpeed = inputDir.x != 0 && inputDir.z != 0 ? Mathf.Max(Mathf.Abs(inputDir.x) , inputDir.z) : inputDir.x != 0 ? Mathf.Abs(inputDir.x) : inputDir.z != 0 ? Mathf.Abs(inputDir.z) : 0;
- 
+        float moveSpeed = inputDir.x != 0 && inputDir.z != 0 ? Mathf.Max(Mathf.Abs(inputDir.x), inputDir.z) : inputDir.x != 0 ? Mathf.Abs(inputDir.x) : inputDir.z != 0 ? Mathf.Abs(inputDir.z) : 0;
+
         anim.SetFloat(speedHash, moveSpeed);
 
-        if (input != Vector2.zero)
-        {
-            Vector3 rotateDir = transform.position + inputDir;
-            rotateDir.y = 0;
-            transform.LookAt(rotateDir);
+        Quaternion camYRotation = Quaternion.Euler(0, Camera.main.transform.rotation.eulerAngles.y, 0);
+        inputDir.y = 0.0f; // 앞서 바닥으로 내려오지 않는 현상 때문에 Y 값을 -2 했기 때문에 값을 다시 초기화 해주지 않으면 x축이 회전하게 된다.
+        inputDir = camYRotation * inputDir;// 카메라의 Y축 회전값을 입력 방향에 곱해서 같이 회전시키기// 생략할 시 월드기준으로 항상 같은 방향으로 회전하게 된다. 
 
-            //if (inputDir.x != 0)
-            //    transform.rotation = Quaternion.Euler(0, inputDir.x * 90, 0);
-            //else if (inputDir.z == -1)
-            //    transform.rotation = Quaternion.Euler(0, 180, 0);
-            //else if (inputDir.z == 1)
-            //    transform.rotation = Quaternion.Euler(0, 0, 0);
-        }
- 
+        targetRotation =  Quaternion.LookRotation(inputDir);
+        //if (input != Vector2.zero)
+        //{
+        //    Vector3 rotateDir = transform.position + inputDir;
+        //    rotateDir.y = 0;
+        //    transform.LookAt(rotateDir);
+
+        //    //if (inputDir.x != 0)
+        //    //    transform.rotation = Quaternion.Euler(0, inputDir.x * 90, 0);
+        //    //else if (inputDir.z == -1)
+        //    //    transform.rotation = Quaternion.Euler(0, 180, 0);
+        //    //else if (inputDir.z == 1)
+        //    //    transform.rotation = Quaternion.Euler(0, 0, 0);
+        //}
+
     }
 
         
