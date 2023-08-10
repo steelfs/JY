997a6bbfd74ec026690dc6b4a01c4c0dd55abd0f
inchimyeong <125725323+steelfs@users.noreply.github.com>
2023-08-10 14:21:19 +0900

    지뢰찾기 프로젝트 시작

---
+++ 06_Mine_Sweeper/Assets/Script/Test/Test_01_Start.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_01_Start : MonoBehaviour
+{
+    //필요클래스
+    // cell
+    // Board
+    // Reset
+    // ImageNumber 
+    //   Timer
+    //   FlagCounter
+}
