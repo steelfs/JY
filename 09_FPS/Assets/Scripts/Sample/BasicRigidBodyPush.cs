af34a4829a966cdf254f87388bc62f02361d01ca
steelfs <125725323+steelfs@users.noreply.github.com>
2023-10-06 11:10:01 +0900

    .

--- 09_FPS/Assets/Scripts/Sample/BasicRigidBodyPush.cs
+++ 09_FPS/Assets/Scripts/Sample/BasicRigidBodyPush.cs
@@ -2,8 +2,8 @@
 
 public class BasicRigidBodyPush : MonoBehaviour
 {
-	public LayerMask pushLayers;
-	public bool canPush;
+	public LayerMask pushLayers;//밀릴 대상의 레이어
+	public bool canPush;//충돌시 밀 수 있는지
 	[Range(0.5f, 5f)] public float strength = 1.1f;
 
 	private void OnControllerColliderHit(ControllerColliderHit hit)
@@ -11,25 +11,41 @@ public class BasicRigidBodyPush : MonoBehaviour
 		if (canPush) PushRigidBodies(hit);
 	}
 
+
+	/// <summary>
+	/// 캐릭터컨트롤러가 이동하다가 충돌이 발생하면 실행되는 이벤트 함수 OnCollision, OnTrigger 랑 비슷하나
+	/// rigidBody 대신 케릭터컨트롤러를 이용하면 이 함수를 사용한다.
+	/// </summary>
+	/// <param name="hit"></param>
 	private void PushRigidBodies(ControllerColliderHit hit)
 	{
 		// https://docs.unity3d.com/ScriptReference/CharacterController.OnControllerColliderHit.html
 
 		// make sure we hit a non kinematic rigidbody
 		Rigidbody body = hit.collider.attachedRigidbody;
-		if (body == null || body.isKinematic) return;
+		if (body == null || body.isKinematic) return;// rigidbody가 없거나 kinematic이면 리턴
 
-		// make sure we only push desired layer(s)
-		var bodyLayerMask = 1 << body.gameObject.layer;
+        // make sure we only push desired layer(s)
+        //설정한 레이어가 아니면 종료
+        // 1 =				0000 0000 0000 0000 0000 0000 0000 0001
+        // 1 << 10 =		0000 0000 0000 0000 0000 0100 0000 0000
+        //pushLayer.value = 0000 0000 0000 0000 0000 0100 0000 0000
+		// 둘을 & 해서 1이나오면 같다. 1이 아니면 서로 다른 레이어이기때문에 return
+        var bodyLayerMask = 1 << body.gameObject.layer;// ex)layer 가 11번째이면 10이 기록되어있다.
 		if ((bodyLayerMask & pushLayers.value) == 0) return;
 
 		// We dont want to push objects below us
+		//너무 아랫쪽으로 밀고있으면 리턴
 		if (hit.moveDirection.y < -0.3f) return;
 
 		// Calculate push direction from move direction, horizontal motion only
+		// x, z 방향으로 밀어낼 값 계산
 		Vector3 pushDir = new Vector3(hit.moveDirection.x, 0.0f, hit.moveDirection.z);
 
 		// Apply the push and take strength into account
+		//실제 적용
 		body.AddForce(pushDir * strength, ForceMode.Impulse);
+
+
 	}
 }
--- 09_FPS/Assets/Scripts/UI/CrossHair.cs
+++ 09_FPS/Assets/Scripts/UI/CrossHair.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class CrossHair : MonoBehaviour
 {
     public AnimationCurve recovryCurve;
+    /// <summary>
+    /// 최대확장크기
+    /// </summary>
     public float maxExpand = 100.0f;
 
+    /// <summary>
+    /// 기본, 최소확장크기
+    /// </summary>
     const float defaultExpand = 30.0f;
     float current = 0;
 
+    /// <summary>
+    /// 조준선들의 RectTransform
+    /// </summary>
     RectTransform[] crossRects;
-    Vector2[] direction = { Vector2.up, Vector2.right, Vector2.down, Vector2.left };
+    readonly Vector2[] direction = { Vector2.up, Vector2.right, Vector2.down, Vector2.left };
     private void Awake()
     {
         crossRects = new RectTransform[transform.childCount];
@@ -20,9 +29,14 @@ public class CrossHair : MonoBehaviour
             crossRects[i] = transform.GetChild(i) as RectTransform; 
         }
     }
+
+    /// <summary>
+    /// 조준선 확장 함수
+    /// </summary>
+    /// <param name="amount">확장되는 정도</param>
     public void Expand(float amount)
     {
-        current = Mathf.Min(current + amount, maxExpand);
+        current = Mathf.Min(current + amount, maxExpand);//확장은 최대치 까지만
         for(int i = 0; i < crossRects.Length; i++)
         {
             crossRects[i].anchoredPosition = (current * direction[i]);
@@ -31,24 +45,29 @@ public class CrossHair : MonoBehaviour
         StopAllCoroutines();
         StartCoroutine(DelayRecovery(0.1f));
     }
+
+    /// <summary>
+    /// 디폴트 위치로 되돌리는 코루틴
+    /// </summary>
+    /// <param name="wait">처음 기다리는 시간</param>
+    /// <returns></returns>
     IEnumerator DelayRecovery(float wait)
     {
-
         yield return new WaitForSeconds(wait);
 
-        float startExpand = current;
-        float time = 0.0f;//1에서 시작해서 0으로 이동
-        float duration = 0.5f;
+        float startExpand = current;//확장된 상태를 최대치로 저장
+        float curveProcess = 0.0f;//커브 진행상황. 1에서 시작해서 0으로 이동
+        float duration = 0.5f; //조준선이 디폴트값까지 이동하는데 걸리는 시간
         float div = 1 / duration;//?  1 / 0.5 = 2;
 
-        while (time < 1)
+        while (curveProcess < 1)//duration초 동안 반복
         {
-            time += Time.deltaTime * div;
-            current = recovryCurve.Evaluate(time) * startExpand;
+            curveProcess += Time.deltaTime * div;
+            current = recovryCurve.Evaluate(curveProcess) * startExpand;//curve를 이용해 current계산
 
             for (int i = 0; i < crossRects.Length; i++)
             {
-                crossRects[i].anchoredPosition = (current + defaultExpand) * direction[i];
+                crossRects[i].anchoredPosition = (current + defaultExpand) * direction[i];//계산 결과대로 점점 축소시키기
             }
             yield return null;
         }
@@ -58,5 +77,4 @@ public class CrossHair : MonoBehaviour
         }
         current = 0;
     }
-
 }
