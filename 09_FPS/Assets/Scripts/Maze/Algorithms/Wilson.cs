ab2145458ec16d2f63b6848ef9c08f08aa59d87c
steelfs <steelfs7@gmail.com>
2023-10-20 22:33:46 +0900

    프로그램 설계도 업데이트

--- 09_FPS/Assets/Scripts/Maze/Algorithms/Wilson.cs
+++ 09_FPS/Assets/Scripts/Maze/Algorithms/Wilson.cs
@@ -22,8 +22,7 @@ public class Wilson : MazeGenerator
     protected override void OnSpecificAlgorithmExcute()
     {
         //생성 후 시리얼 부여 
-        Dictionary<int, WilsonCell> added = new Dictionary<int, WilsonCell>();
-        Dictionary<int, WilsonCell> notAdded = new Dictionary<int, WilsonCell>();
+        Dictionary<int, WilsonCell> confirmed = new Dictionary<int, WilsonCell>();
         int serial = 0;
 
         for (int y = 0; y < height; y++)
@@ -33,79 +32,58 @@ public class Wilson : MazeGenerator
                 WilsonCell cell = new WilsonCell(x,y);
                 cells[GridToIndex(x, y)] = cell;
                 cell.serial = serial++;
-
-                notAdded[cell.serial] = cell;
             }
         }
         WilsonCell arrived = cells[UnityEngine.Random.Range(0, cells.Length)] as WilsonCell;
-        added[arrived.serial] = arrived;// 추가된 리스트에 추가
+        confirmed[arrived.serial] = arrived;// 추가된 리스트에 추가
            
+        //MoveToAdded 제작 필요
+        WilsonCell start = GetNewStartPoint(confirmed);
 
 
-
-        WilsonCell start = GetRandomCell(notAdded);
         Dictionary<int, WilsonCell> path = new Dictionary<int, WilsonCell>();
 
-        Move(start, path, added);
+        Move(start, path, confirmed);
 
-        MakePath(path);
-        //미로에 추가된 부분과, 추가되지않은 그룹을 두개로 만든다
-        //셀을 모두 만든 후 추가되지 않은 그룹에 추가한다.
-        // 그 중 두개를 골라서 하나는 추가된 그룹에 추가하고 하나는 출발지점으로 설정한다.
-        // 사방향 중 랜덤하게, 방문하지 않은 곳으로 움직이면서 딕셔너리에 추가한다.
-        //목적지에 도착시 경로를 미로에 추가시킨다.
+     
     }
 
-    void MakePath(Dictionary<int, WilsonCell> path)
-    {
-        foreach(WilsonCell cell in path.Values)
-        {
-
-        }
-        for (int i = 0; i < path.Count - 2; i++)
-        {
-            WilsonCell current = path[i];
-            WilsonCell next = path[i + 1];
-            ConnectPath(current, next);
-
-        }
-    }
+   
 
     void Move(WilsonCell current, Dictionary<int, WilsonCell> path, Dictionary<int, WilsonCell> added )
     {
-        List<WilsonCell> list = new List<WilsonCell>();
-            
-        WilsonCell next = current;
-        while (!added.ContainsKey(current.serial))//도착지점이 아니면
+        WilsonCell next = null;
+        path[current.serial] = current; //처음은 그냥 추가
+        while (!added.ContainsKey(current.serial))//도착지점이면 종료
         {
-            next = GetNeighbor(next);//랜덤으로 하나 뽑아와서
+            next = GetNeighbor(current);//랜덤으로 하나 뽑아와서
             if (path.ContainsKey(next.serial))// 이미 거쳐온 곳이면 스킵
             {
                 continue;
             }
-            path.Add(next.serial, next);
+            path[next.serial] = next;//경로에 추가
 
             current.next = next;
             current = next;
              
+            //current를 저장후 다음 셀을 next기준으로 정하면 한번에 두칸을 움직이는 형상이 나타난다.
             //path에 포함되어있지 않아야하고, 
         }
 
 
     }
-    private WilsonCell GetRandomCell(Dictionary<int, WilsonCell> notAdded_)//미로에 추가되지 않은 곳 중 랜덤한 셀을 출발지로 리턴
+    private WilsonCell GetNewStartPoint(Dictionary<int, WilsonCell> Added)//미로에 추가되지 않은 곳 중 랜덤한 셀을 출발지로 리턴
     {
-        WilsonCell result = null;
         List<WilsonCell> unVisited = new List<WilsonCell>();
-        foreach (WilsonCell cell in notAdded_.Values)
+        for (int i = 0; i < cells.Length; i++)
         {
-            unVisited.Add(cell);
+            WilsonCell cell = cells[i] as WilsonCell;
+            if (!Added.ContainsValue(cell))
+            {
+                unVisited.Add(cell);
+            }
         }
-        WilsonCell[] arr = unVisited.ToArray();
-        Util.Shuffle(arr);
-        result = arr[UnityEngine.Random.Range(0, arr.Length)];
-
-        return result;
+        return unVisited[UnityEngine.Random.Range(0, unVisited.Count)];
     }
 
 
@@ -124,9 +102,8 @@ public class Wilson : MazeGenerator
                 neighbors.Add(cell);
             }
         }
-        WilsonCell[] arr = neighbors.ToArray();
 
-        return arr[UnityEngine.Random.Range(0, arr.Length)];
+        return neighbors[UnityEngine.Random.Range(0, neighbors.Count)];
     }
 }
 
