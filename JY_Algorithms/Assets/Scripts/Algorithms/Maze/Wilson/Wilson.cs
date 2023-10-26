4ea37096a94b85fc8c965ac8d27d19049d2a8be8
steelfs <steelfs7@gmail.com>
2023-10-26 22:29:12 +0900

    옵시디언 추가

---
+++ JY_Algorithms/Assets/Scripts/Algorithms/Maze/Wilson/Wilson.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class WilsonCell : Cell
+{
+
+    public WilsonCell(int x, int y) : base(x, y)
+    {
+
+    }
+}
+public class Wilson : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
