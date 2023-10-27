e7e257e5cac64727f2d0e18d03f96ba570ed6613
steelfs <125725323+steelfs@users.noreply.github.com>
2023-10-27 14:22:10 +0900

    Enemy Player감지 추가할 차례

--- 09_FPS/Assets/Scripts/Enemy/Enemy.cs
+++ 09_FPS/Assets/Scripts/Enemy/Enemy.cs
@@ -5,13 +5,29 @@ using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.AI;
 
+public enum HitLocation
+{
+    Body,
+    Head,
+    Arm,
+    Leg
+}
+public enum State
+{
+    Patrol,
+    Chase,
+    Attack,
+    Die
+}
 public class Enemy : MonoBehaviour
 {
-    // 1. 플레이어의 총에 맞으면 죽는다.
+    Player player;
     NavMeshAgent agent;
     Vector3 destination;
     int size = CellVisualizer.CellSize;
 
+    public float sightRange = 10;
+    public float sightAngle = 30;
     public float walkSpeed = 5;
     public float runSpeed = 10;
     float speedPenalty = 0;
@@ -31,21 +47,69 @@ public class Enemy : MonoBehaviour
     }
     public float maxHp = 30.0f;
 
+    State state;
+    public State State
+    {
+        get => state;
+        set
+        {
+            state = value;
+            switch (state)
+            {
+                case State.Patrol:
+                    Patrol();
+                    StartCoroutine(DetectCoroutine);
+                    break;
+                case State.Chase:
+                    break;
+                case State.Attack:
+                    break;
+                case State.Die:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
     public Action<Enemy> onDie;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        
     }
     private void OnEnable()
+    {
+    
+    }
+    private void Start()
+    {
+        player = GameManager.Inst.Player;
+        DetectCoroutine = DetectPlayerCoroutine();
+        State = State.Patrol;
+
+    }
+    void Patrol()
     {
         HP = maxHp;
         agent.speed = walkSpeed;
         speedPenalty = 0;
         SetDestination();
     }
+    void Chase()
+    {
 
-   
+    }
+    void Attack()
+    {
+
+    }
+    private void Die()
+    {
+        onDie?.Invoke(this);
+        gameObject.SetActive(false);
+    }
     void SetDestination()
     {
         Vector3 destination = NewDestination();
@@ -69,10 +133,27 @@ public class Enemy : MonoBehaviour
             SetDestination();
         }
     }
-    private void Die()
+    IEnumerator DetectCoroutine;
+    IEnumerator DetectPlayerCoroutine()
     {
-        onDie?.Invoke(this);
-        gameObject.SetActive(false);
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            Vector3 toPlayer = (player.transform.position - transform.position).normalized;
+            float angle = Vector3.Angle(transform.forward, toPlayer);
+
+            if (angle < sightAngle / 2)
+            { // 시야각 체크
+                if (Physics.Raycast(transform.position, toPlayer, out RaycastHit hit, sightRange))
+                {
+                    if (hit.collider.tag == "Player")
+                    {
+                        // 플레이어 발견!
+                        agent.SetDestination(player.transform.position);
+                    }
+                }
+            }
+        }
     }
     public void OnAttacked(HitLocation hitLocation, float damage)
     {
@@ -100,10 +181,11 @@ public class Enemy : MonoBehaviour
       
     }
 }
-public enum HitLocation
-{
-    Body,
-    Head,
-    Arm,
-    Leg
-}
+
+
+//상태를 가진다.
+// 순찰 : 랜덤으로 계속 이동, 추적
+// : 플레이어가 시야에 들어오면 추적상태가 되어서 마지막으로 목격한 위치를 목적지로 도착후 ,
+// 공격 : 추적상태에서 공격범위 안으로 들어왔을 때 공격시작,
+// 사망 : 일정시간 지나서 부활 후 순찰상태
+
