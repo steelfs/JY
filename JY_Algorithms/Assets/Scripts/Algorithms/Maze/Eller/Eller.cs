7e0e2c22c1dd16db2746fc7c051a3b599633543d
steelfs <125725323+steelfs@users.noreply.github.com>
2023-10-31 11:25:37 +0900

    Update Eller.cs

--- JY_Algorithms/Assets/Scripts/Algorithms/Maze/Eller/Eller.cs
+++ JY_Algorithms/Assets/Scripts/Algorithms/Maze/Eller/Eller.cs
@@ -36,25 +36,61 @@ public class Eller : MazeGenerator
     }
     public override async void MakeMaze()
     {
+        List<EllerCell> row = new List<EllerCell>();
         for (int y = 0; y < height; y++)
         {
+            row.Clear();
             for (int x = 0; x < width - 1; x++)
             {
-                MergeCellRow(cells[GridToIndex(x, y)] as EllerCell, cells[GridToIndex(x + 1, y)] as EllerCell);
+                EllerCell current = cells[GridToIndex(x, y)] as EllerCell;
+                EllerCell next = cells[GridToIndex(x + 1, y)] as EllerCell;
+                MergeCell(current, next, 0.5f);
+                row.Add(current);
+                row.Add(next);
                 await Task.Delay(500);
             }
+            row = row.Distinct().ToList();
+            EllerCell[] arr = row.ToArray();
+            Util.Shuffle(arr);
+            MergeCellColumn(arr);
         }
     }
-    void MergeCellColumn(EllerCell from, EllerCell to)
+    void MergeCellColumn(EllerCell[] row)
     {
+        EllerCell[] belows = new EllerCell[row.Length];
+        for (int i = 0; i < row.Length; i++)
+        {
+            EllerCell below = cells[GridToIndex(row[i].X, row[i].Y + 1)] as EllerCell;
+            belows[i] = below;
+        }
+        for (int i = 0; i < row.Length; i++)
+        {
+            int mergeCount = 0;
+            for (int j = 0; j < belows.Length; j++)
+            {
+                if (sets.ContainsKey(belows[j].group))// 이미 하나 이상 윗쪽 그룹과 합쳐진 경우
+                {
+                    mergeCount++;
+                }
+                else
+                {
+                    //아직 합쳐지지 않은 경우
+                    continue;
+                }
+            }
+            if (mergeCount > 0)
+            {
+
+            }
+        }
         // 호출하기 전 같은 행의 셀들을 모아서 한번 섞은 다음 차례로 이 함수를 호출한다.
         //만약 같은 행에 있는 셀들 줄 sets에 포함되어있는게 있다면 확률적으로 머지하고 없다면 100 프로확률로 머지한다.
     }
-    void MergeCellRow(EllerCell from, EllerCell to)
+    void MergeCell(EllerCell from, EllerCell to, float successRate)
     {
         if (from.group != to.group)
         {
-            if (UnityEngine.Random.value > 0.5f)
+            if (UnityEngine.Random.value < successRate)
             {
                 to.group = from.group;
                 if (!sets.ContainsKey(from.group))
