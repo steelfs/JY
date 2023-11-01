1b57675760561d718160f8eb2ebb5dd605842f43
steelfs <125725323+steelfs@users.noreply.github.com>
2023-11-01 13:12:42 +0900

    Row방향 머지시 모든셀을 같은그룹에 병합 필요

--- JY_Algorithms/Assets/Scripts/Algorithms/Maze/Eller/Eller.cs
+++ JY_Algorithms/Assets/Scripts/Algorithms/Maze/Eller/Eller.cs
@@ -18,7 +18,10 @@ public class EllerCell : Cell
 }
 public class Eller : MazeGenerator
 {
+    
     Dictionary<int, List<EllerCell>> sets = new Dictionary<int, List<EllerCell>>();
+    EllerCell previous_From = null;
+    EllerCell previous_To = null;
     public override Cell[] MakeCells(int width, int height)
     {
         this.width = width;
@@ -44,73 +47,94 @@ public class Eller : MazeGenerator
             row.Clear();
             for (int x = 0; x < width; x++)
             {
-                if(IsInGrid(x - 1, y))
-                {
-                    on_Set_DefaultMaterial?.Invoke(GridToIndex(x - 1, y));
-                }
                 EllerCell current = cells[GridToIndex(x, y)] as EllerCell;
                 EllerCell next = cells[GridToIndex(Mathf.Min(x + 1, width - 1), y)] as EllerCell;
-                on_Set_PathMaterial?.Invoke(GridToIndex(current.X, current.Y));
-                on_Set_PathMaterial?.Invoke(GridToIndex(next.X, next.Y));
-
-                MergeCell(current, next, 0.5f);
+                if (y == height - 1)
+                {
+                    MergeCell(current, next, 1.1f);
+                }
+                else if (current.group != next.group)
+                {
+                    MergeCell(current, next, 0.7f);
+                }
                 row.Add(current);
                 row.Add(next);
-                await Task.Delay(1000);
+                await Task.Delay(2000);
             }
             row = row.Distinct().ToList();
-            EllerCell[] arr = row.ToArray();
-            foreach (EllerCell cell in arr)
+            foreach (EllerCell cell in row)
             {
                 on_Set_DefaultMaterial?.Invoke(GridToIndex(cell.X, cell.Y));
                 await Task.Delay(50);
             }
-            Util.Shuffle(arr);
-            await MergeCellColumn(arr);
-        }
-    }
-    //병합되지 않은 셀 역시 sets에 포함되어야 한다.
-    //j가 항상 0이어서 같은 셀을 기준으로 sets에 추가되어있는지 확인하는 문제
-    async Task MergeCellColumn(EllerCell[] row)
-    {
-        EllerCell[] belows = new EllerCell[row.Length];
-        for (int i = 0; i < row.Length; i++)
-        {
-            EllerCell below = cells[GridToIndex(row[i].X, row[i].Y + 1)] as EllerCell;//index out of range
-            belows[i] = below;
-        }
-        foreach(List<EllerCell> set in sets.Values.ToList())
-        {
-            int i = 0;
-            while (i < set.Count)
+            if (y < height - 1)
             {
-                EllerCell current = set[i];
-                EllerCell belowCell = cells[GridToIndex(set[i].X, set[i].Y + 1)] as EllerCell;
-                if (set.Count != 1)//카운트가 남았으면 50%확률로 머지
-                {
-                    MergeCell(current, belowCell, 0.5f);
-                }
-                else//카운트가 1이면 (마지막 셀이면) 100%확률로 머지
-                {
-                    MergeCell(current, belowCell, 1);
-                }
-                on_Set_DefaultMaterial?.Invoke(GridToIndex(set[Mathf.Max(0, i - 1)].X, set[Mathf.Max(0, i - 1)].Y));// 이전에 셋팅된 머티리얼 되돌리기
-                on_Set_DefaultMaterial?.Invoke(GridToIndex(set[Mathf.Max(0, i - 1)].X, set[Mathf.Max(0, i - 1)].Y + 1));
-                on_Set_PathMaterial?.Invoke(GridToIndex(current.X, current.Y));//현재 진행될 셀의 머티리얼 설정
-                on_Set_PathMaterial?.Invoke(GridToIndex(belowCell.X, belowCell.Y));
-                await Task.Delay(1000);
-                i++;
+                await MergeCellColumn(row);
             }
         }
-        
+    }
+    async Task MergeCellColumn(List<EllerCell> row)
+    {
+       
+        for (int i = 0; i < row.Count; i++)
+        {
+            List<EllerCell> set = null;
+            if (sets.ContainsKey(row[i].group))
+            {
+                set = sets[row[i].group];
+            }
+            else
+            {
+                continue;
+            }
+            if (set.Count > 1)//그룹에 추가된 갯수가 1개 이상일 때 최소 하나는 무조건 머지
+            {
+                int repeat = set.Count;
+                for (int j = 0; j < repeat; j++)
+                {
+                    EllerCell current = set[j];
+                    EllerCell below = cells[GridToIndex(set[j].X, set[j].Y + 1)] as EllerCell;
+                    if (j == 0)
+                    {
+                        MergeCell(current, below, 1.1f);
+                        await Task.Delay(500);
+                    }
+                    else
+                    {
+                        MergeCell(current, below, 0.3f);
+                        await Task.Delay(500);
+                    }
+                }
+            }
+            else//100% 확률로 머지
+            {
+                EllerCell current = set[0];
+                EllerCell below = cells[GridToIndex(set[0].X, set[0].Y + 1)] as EllerCell;
+                MergeCell(current, below, 1.1f);
+
+                await Task.Delay(500);
+            }
+            sets.Remove(set[0].group);//같은 그룹을 또다시 도는 것을 방지 하기 위해 추가했는데
+            //이 코드로 인해 가로방향 으로 머지될 때 연결되어있는 셀들의 그룹을 모두 같은 그룹으로 병합해야하는데 
+            //리스트가 이미 지워진 문제 발생
+        }
     }
     void MergeCell(EllerCell from, EllerCell to, float successRate)
     {
+        if (previous_From != null)
+        {
+            on_Set_DefaultMaterial?.Invoke(GridToIndex(previous_From.X, previous_From.Y));
+            on_Set_DefaultMaterial?.Invoke(GridToIndex(previous_To.X, previous_To.Y));
+        }
+        on_Set_NextMaterial?.Invoke(GridToIndex(from.X, from.Y));
+        on_Set_NextMaterial?.Invoke(GridToIndex(to.X, to.Y));
+        previous_From = from;
+        previous_To = to;
         if (!sets.ContainsKey(from.group))//리스트가 없을경우 생성
         {
             sets[from.group] = new List<EllerCell>();
         }
-        if (UnityEngine.Random.value < successRate)//50%확률로 머지
+        if (UnityEngine.Random.value < successRate)
         {
             to.group = from.group;
             sets[from.group].Add(from);
@@ -127,6 +151,7 @@ public class Eller : MazeGenerator
                 sets[to.group] = new List<EllerCell>();
             }
             sets[to.group].Add(to);
+            
         }
         sets[from.group] = sets[from.group].Distinct().ToList();//중복된 요소 제거하기
         sets[to.group] = sets[to.group].Distinct().ToList();
--- JY_Algorithms/Assets/Scripts/Test/TestObject.cs
+++ JY_Algorithms/Assets/Scripts/Test/TestObject.cs
@@ -6,14 +6,22 @@ using UnityEngine.InputSystem;
 
 public class TestObject : TestBase
 {
-    public CellVisualizer cellVisualizer;
     
     protected override void Test1(InputAction.CallbackContext context)
     {
+        PlayerPrefs.DeleteAll();
     }
     protected override void Test2(InputAction.CallbackContext context)
     {
-        cellVisualizer.OnSet_Default_Material();
+        PlayerPrefs.SetFloat("HP", 50.5f);
     }
-  
+    protected override void Test3(InputAction.CallbackContext context)
+    {
+        //float hp = PlayerPrefs.GetFloat("HP");
+        //Debug.Log(hp);
+
+        bool result = PlayerPrefs.HasKey("MP");
+        Debug.Log($"MP 존재 여부 = {result}");
+    }
+
 }
