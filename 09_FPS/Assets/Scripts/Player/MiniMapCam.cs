c63ad3a92a2e47e1c8c7c3cb366b530c4a11bec1
steelfs <125725323+steelfs@users.noreply.github.com>
2023-11-07 12:20:26 +0900

    FPS 완료

--- 09_FPS/Assets/Scripts/Player/MiniMapCam.cs
+++ 09_FPS/Assets/Scripts/Player/MiniMapCam.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MiniMapCam : MonoBehaviour
 {
     Player player;
+    Vector3 newPos;
+
+    public float smooth = 2;
     private void Start()
     {
         player = GameManager.Inst.Player;
     }
     private void Update()
     {
-        transform.Translate(player.transform.position.x, player.transform.position.y + 30, player.transform.position.z);
+       // transform.Translate(player.transform.position.x, player.transform.position.y + 30, player.transform.position.z);
+        newPos.x = player.transform.position.x;
+        newPos.y = player.transform.position.y + 40;
+        newPos.z = player.transform.position.z;
+        transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * smooth);
     }
+    
 }
