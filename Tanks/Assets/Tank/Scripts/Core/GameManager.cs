5263b8400f54ebe06e277191343596d420ea5703
steelfs <125725323+steelfs@users.noreply.github.com>
2023-11-07 13:02:12 +0900

    Tank - 프로젝트 초기세팅

---
+++ Tanks/Assets/Tank/Scripts/Core/GameManager.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager_ : Singleton<GameManager_>
+{
+
+    protected override void OnInitialize()
+    {
+        base.OnInitialize();
+
+       
+    }
+ 
+    public void AddKillCount()
+    {
+    }
+}
